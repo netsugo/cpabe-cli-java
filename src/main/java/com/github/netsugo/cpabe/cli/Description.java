bda51746cae17638cffa1c17b0a41b37982fa386
netsugo <netsugo@users.noreply.github.com>
2021-04-04 22:11:04 +0900

    update descriptions

--- src/main/java/com/github/netsugo/cpabe/cli/Description.java
+++ src/main/java/com/github/netsugo/cpabe/cli/Description.java
@@ -1,20 +1,20 @@
 package com.github.netsugo.cpabe.cli;
 
 public class Description {
-    public final static String decrypted = "plain file";
-    public final static String encrypted = "encrypted file";
-    public final static String publicKey = "public key file";
-    public final static String privateKey = "private key file";
-    public final static String master = "master key file";
+    public final static String decrypted = "plain file path";
+    public final static String encrypted = "encrypted file path";
+    public final static String publicKey = "public key file path";
+    public final static String privateKey = "private key file path";
+    public final static String master = "master key file path";
     public final static String policy = "policy text";
     public final static String attr = "attribute text";
-    public final static String policyExample = "example: ";
-    public final static String attrExample = "example: ";
+    public final static String policyExample = "Format: \"key1:value1 key2:value2 ... MofN\"";
+    public final static String attrExample = "Format: \"key1:value1 key2:value2 ...\"";
 
     public static class Command {
-        public final static String setup = "generate public key and master key";
-        public final static String keygen = "generate private key";
-        public final static String encrypt = "encrypt plain(decrypted) file";
-        public final static String decrypt = "decrypt encrypted file";
+        public final static String setup = "Generate public key and master key.";
+        public final static String keygen = "Generate private key.";
+        public final static String encrypt = "Encrypt plain(decrypted) data.";
+        public final static String decrypt = "Decrypt encrypted data.";
     }
 }
